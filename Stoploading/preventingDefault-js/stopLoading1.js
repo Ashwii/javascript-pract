64474da0838973955252c15497943135975fb507
Ashwii <ashwinianandhraj@gmail.com>
2018-01-26 22:12:04 +0530

    using only pure js

---
+++ Stoploading/preventingDefault-js/stopLoading1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var x = document.getElementById("stuck");
+x.addEventListener("click",function(stk){
+	stk.preventDefault();
+	alert("This page is unavailabele... Thank you!!!");
+});
