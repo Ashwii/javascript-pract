e7a51a2b7e74a4a665c2010bf987a0fca55e309f
Ashwii <ashwinianandhraj@gmail.com>
2018-01-27 07:27:01 +0530

    Using only js and the class name used addEventListener

--- Stoploading/preventingDefault-js/stopLoading1.js
+++ Stoploading/preventingDefault-js/stopLoading1.js
@@ -1,7 +1,11 @@
 "use strict";
 
-var x = document.getElementById("stuck");
-x.addEventListener("click",function(stk){
+var x = document.getElementsByClassName("stuck");
+/*"stuck"(class name) is a Node type..Need to loop overall the element and 
+attach the listener to each one in the list */
+Array.from(x).forEach(function(xx){
+xx.addEventListener("click",function(stk){
 	stk.preventDefault();
 	alert("This page is unavailabele... Thank you!!!");
+});
 });
